5b28c0adc43ad53f119a1efc3ff41ad448c9db3b
Laurence Foley <x15047911@student.ncirl.ie>
2017-11-29 15:46:09 +0000

    create view engine for navigating between different jframe forms

--- src/rugbyapp/ViewEngine.java
+++ src/rugbyapp/ViewEngine.java
@@ -12,37 +12,41 @@ package rugbyapp;
 public class ViewEngine {
     
     // Views
-    Login loginGUI = new Login();
-    AddProductGUI addProductGUI = new AddProductGUI();
-    ViewProductsGUI viewProductsGUI = new ViewProductsGUI();
+    Login loginGUI = new Login(this);
+    AddProductGUI addProductGUI = new AddProductGUI(this);
+    ViewProductsGUI viewProductsGUI = new ViewProductsGUI(this);
     
     public ViewEngine() {
-        loginGUI.setVisible(true);
-        addProductGUI.setVisible(true);
-        viewProductsGUI.setVisible(true);
+        loginGUI.setVisible(false);
+        addProductGUI.setVisible(false);
+        viewProductsGUI.setVisible(false);
     }
     
     public void render(String viewName) {
         switch(viewName) {
             case "login" :
                loginGUI.setVisible(true);
-               addProductGUI.setVisible(true);
-               viewProductsGUI.setVisible(true);
+               addProductGUI.setVisible(false);
+               viewProductsGUI.setVisible(false);
                // Statements
                break; // optional
 
-            case "add" :
-               loginGUI.setVisible(true);
+            case "addProduct" :
+               loginGUI.setVisible(false);
                addProductGUI.setVisible(true);
-               viewProductsGUI.setVisible(true);
+               viewProductsGUI.setVisible(false);
                break;
+            
+            case "viewProduct" :
+                loginGUI.setVisible(false);
+                addProductGUI.setVisible(false);
+                viewProductsGUI.setVisible(true);
+            break;
             
             default :
                throw new Error("Wrong Filed name");
          }
-        loginGUI.setVisible(true);
-        loginGUI.setVisible(true);
-        loginGUI.setVisible(true);
+ 
     }
     
     
